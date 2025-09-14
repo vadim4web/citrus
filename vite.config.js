693b1465ae89aa8c3c04be6df8cab01fd09d9bc0
vadim4web <vadim4web@gmail.com>
2025-09-14 23:02:20 +0300

    simple deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
+// dynamic base depending on environment
+export default defineConfig(({ command }) => ({
   plugins: [vue()],
-})
+  base: command === 'build'
+    ? '/cssscope/'  // repo name for GitHub Pages
+    : '/',          // local dev
+}))
